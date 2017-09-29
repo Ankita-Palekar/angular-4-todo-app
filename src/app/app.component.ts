3b1bc1bf30f9dee08626ce75d5347c33fc427857
ankita <ankita@cdmx.in>
2017-09-29 19:06:11 +0530

    - App components added to the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoDataService} from './todo-data.service';
+import {Todo} from './todo';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private todoDataService: TodoDataService) {
+
+  }
+
+  newTodo: Todo = new Todo();
+
+  addTodo() {
+    this.todoDataService.addRecord(this.newTodo);
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleComplete(todo);
+  }
+
+  removeTodo(todo){
+    this.todoDataService.deleteRecord(todo.id);
+  }
+
+  get todos(){
+    return this.todoDataService.getAll();
+  }
 }
